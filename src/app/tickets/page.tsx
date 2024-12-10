de9816d982197b79af2e8ac2107bb1d06c7732a1
Takuma Yoshimi <ysm0706glee@gmail.com>
2024-12-10 17:13:02 +0100

    typescript

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -2,11 +2,18 @@ import Link from "next/link";
 import { initialTickets } from "@/data";
 import { ticketPath } from "@/paths";
 
+const TICKET_ICONS = {
+  OPEN: "O",
+  IN_PROGRESS: ">",
+  DONE: "X",
+};
+
 const TicketsPage = () => {
   return (
     <div>
       {initialTickets.map((ticket) => (
         <div key={ticket.id}>
+          <div>{TICKET_ICONS[ticket.status]}</div>
           <h2 className="text-lg">{ticket.title}</h2>
           <Link href={ticketPath(ticket.id)} className="text-sm underline">
             View
--- src/data.ts
+++ src/data.ts
@@ -3,12 +3,18 @@ export const initialTickets = [
     id: "1",
     title: "Ticket 1",
     content: "This is the first ticket.",
-    status: "DONE",
+    status: "DONE" as const,
   },
   {
     id: "2",
     title: "Ticket 2",
     content: "This is the second ticket.",
-    status: "OPEN",
+    status: "OPEN" as const,
+  },
+  {
+    id: "3",
+    title: "Ticket ",
+    content: "This is the third ticket.",
+    status: "IN_PROGRESS" as const,
   },
 ];
