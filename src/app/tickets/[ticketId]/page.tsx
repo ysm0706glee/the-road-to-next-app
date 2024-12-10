0fdbd9e3859cf8dacd8acf28970a531c4e1df438
Takuma Yoshimi <ysm0706glee@gmail.com>
2024-12-10 17:03:09 +0100

    file-based routing

---
+++ src/app/tickets/[ticketId]/page.tsx
@@ -0,0 +1,24 @@
+import { initialTickets } from "@/data";
+
+type TicketPageProps = {
+  params: {
+    ticketId: string;
+  };
+};
+
+const TicketPage = ({ params }: TicketPageProps) => {
+  const ticket = initialTickets.find((ticket) => ticket.id === params.ticketId);
+
+  if (!ticket) {
+    return <div>Ticket not found</div>;
+  }
+
+  return (
+    <div>
+      <h2 className="text-lg">{ticket.title}</h2>
+      <h2 className="text-sm">{ticket.content}</h2>
+    </div>
+  );
+};
+
+export default TicketPage;
---
+++ src/data.ts
@@ -0,0 +1,14 @@
+export const initialTickets = [
+  {
+    id: "1",
+    title: "Ticket 1",
+    content: "This is the first ticket.",
+    status: "DONE",
+  },
+  {
+    id: "2",
+    title: "Ticket 2",
+    content: "This is the second ticket.",
+    status: "OPEN",
+  },
+];
