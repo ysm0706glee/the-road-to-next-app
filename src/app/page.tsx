c7ab2a6b8316ab8bc69d8177399ced956b81ea65
Takuma Yoshimi <ysm0706glee@gmail.com>
2024-12-12 14:11:41 +0100

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,18 @@ import { ticketsPath } from "@/paths";
 
 const HomePage = () => {
   return (
-    <div>
-      <h2>Home Page</h2>
-      <Link href={ticketsPath()} className="underline">
-        Go to Tickets
-      </Link>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">Home Page</h2>
+        <p className="text-sm text-muted-foreground">
+          Your home place to start
+        </p>
+      </div>
+      <div className="flex-1 flex flex-col items-center">
+        <Link href={ticketsPath()} className="underline">
+          Go to Tickets
+        </Link>
+      </div>
     </div>
   );
 };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,15 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        "fade-in-from-top": {
+          from: { opacity: "0", transform: "translateY(-16px)" },
+          to: { opacity: "1", transform: "translateY(0)" },
+        },
+      },
+      animation: {
+        "fade-in-from-top": "fade-in-from-top 0.5s ease-out",
+      },
     },
   },
   plugins: [],
