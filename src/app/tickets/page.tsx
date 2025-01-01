2456790fdcbe56cf36bab44d3f8dca19290d3806
Takuma Yoshimi <ysm0706glee@gmail.com>
2025-01-01 14:26:19 +0900

    server vs client components

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,8 +1,22 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import { Heading } from "@/components/heading";
-import { initialTickets } from "@/data";
 import { TicketItem } from "@/features/ticket/components/ticket-item";
+import { getTickets } from "@/features/ticket/queries/get-tickets";
+import { Ticket } from "@/features/ticket/types";
 
 const TicketsPage = () => {
+  const [tickets, setTickets] = useState<Ticket[]>([]);
+
+  useEffect(() => {
+    const fetchTickets = async () => {
+      const result = await getTickets();
+      setTickets(result);
+    };
+    fetchTickets();
+  }, []);
+
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading
@@ -11,7 +25,7 @@ const TicketsPage = () => {
       />
 
       <div className="flex-1 flex flex-col items-center gap-y-4 animate-fade-in-from-top">
-        {initialTickets.map((ticket) => (
+        {tickets.map((ticket) => (
           <TicketItem key={ticket.id} ticket={ticket} />
         ))}
       </div>
---
+++ src/features/ticket/queries/get-tickets.ts
@@ -0,0 +1,11 @@
+import { initialTickets } from "@/data";
+import { Ticket } from "../types";
+
+export const getTickets = async (): Promise<Ticket[]> => {
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2000);
+  });
+  return new Promise((resolve) => {
+    resolve(initialTickets);
+  });
+};
