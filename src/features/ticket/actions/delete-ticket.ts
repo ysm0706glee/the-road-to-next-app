ce74d17f5ba4c676f6263f64c62d2f12a1719a39
Takuma Yoshimi <ysm0706glee@gmail.com>
2025-01-15 09:17:39 +0900

    redirect

--- src/features/ticket/actions/delete-ticket.ts
+++ src/features/ticket/actions/delete-ticket.ts
@@ -1,7 +1,10 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import { prisma } from "@/lib/prisma";
+import { ticketsPath } from "@/paths";
 
 export const deleteTicket = async (id: string) => {
   await prisma.ticket.delete({ where: { id } });
+  redirect(ticketsPath());
 };
