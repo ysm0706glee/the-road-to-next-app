c2b3e0e130477ea3a0c03d011206506c20ed23be
Takuma Yoshimi <ysm0706glee@gmail.com>
2025-06-23 16:53:02 +0900

    password strength meter

--- src/features/password/components/password-reset-form.tsx
+++ src/features/password/components/password-reset-form.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { useActionState } from "react";
+import { useActionState, useState } from "react";
+import zxcvbn from "zxcvbn";
 import { FieldError } from "@/components/form/field-error";
 import { Form } from "@/components/form/form";
 import { SubmitButton } from "@/components/form/submit-button";
 import { EMPTY_ACTION_STATE } from "@/components/form/utils/to-action-state";
 import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
 import { passwordReset } from "../actions/password-reset";
 
 type PasswordResetFormProps = {
@@ -18,6 +20,14 @@ const PasswordResetForm = ({ tokenId }: PasswordResetFormProps) => {
     EMPTY_ACTION_STATE
   );
 
+  const [password, setPassword] = useState(
+    (actionState.payload?.get("password") as string) || ""
+  );
+
+  const strength = zxcvbn(password).score * 25;
+
+  const feedback = zxcvbn(password).feedback;
+
   return (
     <Form action={action} actionState={actionState}>
       <Input
@@ -25,7 +35,10 @@ const PasswordResetForm = ({ tokenId }: PasswordResetFormProps) => {
         name="password"
         placeholder="Password"
         defaultValue={actionState.payload?.get("password") as string}
+        onChange={(e) => setPassword(e.target.value)}
       />
+      <Progress value={strength} />
+      {feedback.warning && <p>{feedback.warning}</p>}
       <Input
         type="password"
         name="confirmPassword"
