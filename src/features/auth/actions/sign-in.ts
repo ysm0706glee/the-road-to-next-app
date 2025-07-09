3f1e14e18204808a59cb07e6e7e2132faa8c5dd8
Takuma Yoshimi <ysm0706glee@gmail.com>
2025-07-10 08:23:59 +0900

    fix sign-in action

--- src/features/auth/actions/sign-in.ts
+++ src/features/auth/actions/sign-in.ts
@@ -1,7 +1,5 @@
 "use server";
 
-import { verify } from "@node-rs/argon2";
-import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { z } from "zod";
 import {
@@ -9,9 +7,12 @@ import {
   fromErrorToActionState,
   toActionState,
 } from "@/components/form/utils/to-action-state";
-import { lucia } from "@/lib/lucia";
+import { verifyPasswordHash } from "@/features/password/utils/hash-and-verify";
+import { createSession } from "@/lib/lucia";
 import { prisma } from "@/lib/prisma";
 import { ticketsPath } from "@/paths";
+import { generateRandomToken } from "@/utils/crypto";
+import { setSessionCookie } from "../utils/session-cookie";
 
 const signInSchema = z.object({
   email: z.string().min(1, { message: "Is required" }).max(191).email(),
@@ -23,27 +24,28 @@ export const signIn = async (_actionState: ActionState, formData: FormData) => {
     const { email, password } = signInSchema.parse(
       Object.fromEntries(formData)
     );
+
     const user = await prisma.user.findUnique({
-      where: {
-        email,
-      },
+      where: { email },
     });
+
     if (!user) {
-      return toActionState("ERROR", "User not found", formData);
+      return toActionState("ERROR", "Incorrect email or password", formData);
     }
-    const validPassword = await verify(user.passwordHash, password);
+
+    const validPassword = await verifyPasswordHash(user.passwordHash, password);
+
     if (!validPassword) {
-      return toActionState("ERROR", "Invalid password", formData);
+      return toActionState("ERROR", "Incorrect email or password", formData);
     }
-    const session = await lucia.createSession(user.id, {});
-    const sessionCookie = lucia.createSessionCookie(session.id);
-    (await cookies()).set(
-      sessionCookie.name,
-      sessionCookie.value,
-      sessionCookie.attributes
-    );
+
+    const sessionToken = generateRandomToken();
+    const session = await createSession(sessionToken, user.id);
+
+    await setSessionCookie(sessionToken, session.expiresAt);
   } catch (error) {
     return fromErrorToActionState(error, formData);
   }
+
   redirect(ticketsPath());
 };
