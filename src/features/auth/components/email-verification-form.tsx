710fd8311fbf72cf99037e84a22c9f826ffe1e86
Takuma Yoshimi <ysm0706glee@gmail.com>
2025-07-11 10:54:46 +0900

    mask the verification code input

--- src/features/auth/components/email-verification-form.tsx
+++ src/features/auth/components/email-verification-form.tsx
@@ -1,11 +1,16 @@
 "use client";
 
+import { REGEXP_ONLY_DIGITS_AND_CHARS } from "input-otp";
 import { useActionState } from "react";
 import { FieldError } from "@/components/form/field-error";
 import { Form } from "@/components/form/form";
 import { SubmitButton } from "@/components/form/submit-button";
 import { EMPTY_ACTION_STATE } from "@/components/form/utils/to-action-state";
-import { Input } from "@/components/ui/input";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
 import { emailVerification } from "../actions/email-verification";
 
 const EmailVerificationForm = () => {
@@ -16,11 +21,23 @@ const EmailVerificationForm = () => {
 
   return (
     <Form action={action} actionState={actionState}>
-      <Input
+      <InputOTP
+        maxLength={8}
+        pattern={REGEXP_ONLY_DIGITS_AND_CHARS}
         name="code"
-        placeholder="Code"
         defaultValue={actionState.payload?.get("code") as string}
-      />
+      >
+        <InputOTPGroup>
+          <InputOTPSlot index={0} />
+          <InputOTPSlot index={1} />
+          <InputOTPSlot index={2} />
+          <InputOTPSlot index={3} />
+          <InputOTPSlot index={4} />
+          <InputOTPSlot index={5} />
+          <InputOTPSlot index={6} />
+          <InputOTPSlot index={7} />
+        </InputOTPGroup>
+      </InputOTP>
       <FieldError actionState={actionState} name="code" />
       <SubmitButton label="Verify Email" />
     </Form>
