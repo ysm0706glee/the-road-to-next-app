678f5c889fb28f76d8089bf6c2d51f9016e031a8
Takuma Yoshimi <ysm0706glee@gmail.com>
2026-01-10 10:06:30 +0900

    clean up orphaned S3 files

--- src/features/organization/actions/delete-organization.ts
+++ src/features/organization/actions/delete-organization.ts
@@ -5,6 +5,7 @@ import {
   toActionState,
 } from "@/components/form/utils/to-action-state";
 import { getAdminOrRedirect } from "@/features/membership/queries/get-admin-or-redirect";
+import { inngest } from "@/lib/inngest";
 import { prisma } from "@/lib/prisma";
 import { getOrganizationsByUser } from "../queries/get-organizations-by-user";
 
@@ -23,6 +24,29 @@ export const deleteOrganization = async (organizationId: string) => {
     }
 
     await prisma.organization.delete({ where: { id: organizationId } });
+
+    const attachments = await prisma.attachment.findMany({
+      where: {
+        ticket: {
+          organizationId,
+        },
+      },
+      include: {
+        ticket: true,
+      },
+    });
+
+    for (const attachment of attachments) {
+      await inngest.send({
+        name: "app/attachment.deleted",
+        data: {
+          organizationId: attachment.ticket.organizationId,
+          ticketId: attachment.ticketId,
+          fileName: attachment.name,
+          attachmentId: attachment.id,
+        },
+      });
+    }
   } catch (error) {
     return fromErrorToActionState(error);
   }
